3f76804d8dfcb6d78fce0b1820fe7270b33d1b40
Yaroslav Ovdii <yaovdiy@gmail.com>
2024-05-01 16:51:08 +0300

    updated UI for the TODOs

---
+++ src/lib/components/Toasts.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { fade } from 'svelte/transition';
+	import type { Toast } from '$lib';
+	const { toasts }: { toasts: Toast[] } = $props();
+</script>
+
+{#snippet tost(toast: Toast)}
+	<div out:fade class={`alert alert-${toast.type}`}>
+		<span>{toast.content}</span>
+	</div>
+{/snippet}
+
+<div class="toast toast-end">
+	{#each toasts as toast}
+		{@render tost(toast)}
+	{/each}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,20 @@
 <script lang="ts">
 	import db, { type TODO } from '$lib/db';
+	import { onMount } from 'svelte';
+	import type { Toast } from '$lib';
+	import Toasts from '$lib/components/Toasts.svelte';
 
+	let loading = $state(false);
 	let todos = $state<TODO[]>([]);
 	let newTodoName = $state<string>('');
 	let saving = $state<boolean>(false);
+	let toasts = $state<Toast[]>([]);
+	let savedInDbTodos = $state<TODO[]>([]);
 	let done = $derived<TODO[]>(todos.filter((todo) => todo.done));
 	let notDone = $derived<TODO[]>(todos.filter((todo) => !todo.done));
 	let canSave = $derived<boolean>(todos.length > 0);
 	let canLoad = $derived<boolean>(todos.length === 0);
+	let interval: ReturnType<typeof setInterval> = 0;
 
 	async function loadFromDB() {
 		if (!canLoad) {
@@ -15,30 +22,71 @@
 		}
 		const dbTodos = await db.todos.toArray();
 
+		if (dbTodos.length) {
+			showToast({ type: 'success', content: 'We haved loaded previosly saved Todo list for you' });
+		} else {
+			showToast({ type: 'info', content: 'There was no saved Todo list in this browser' });
+		}
 		todos = dbTodos;
+		savedInDbTodos = dbTodos;
 	}
 
-	async function saveToDB(e: Event) {
-		e.preventDefault();
+	async function saveToDB() {
 		if (!canSave) {
 			return;
 		}
 
 		saving = true;
+		const getDBData = await db.todos.toArray();
+
 		const savePromises: Promise<any>[] = [];
 		todos.forEach((todo) => {
-			savePromises.push(db.todos.add({ ...todo }));
+			if (!getDBData.length) {
+				savePromises.push(db.todos.add({ ...todo }));
+				return;
+			}
+
+			const todoInTheList = getDBData.find((dbTodo) => dbTodo.id === todo.id);
+
+			if (!todoInTheList) {
+				savePromises.push(db.todos.add({ ...todo }));
+				return;
+			}
+
+			savePromises.push(db.todos.update(todo.id, todo));
 		});
 
 		await Promise.allSettled(savePromises);
 
+		await loadFromDB();
 		saving = false;
-
-		alert('Saved locally');
+		showToast({ type: 'success', content: 'You have saved current Todo list to the DB' });
 	}
 
-	function addTodo(e: Event) {
-		e.preventDefault();
+	onMount(async () => {
+		loading = true;
+		await loadFromDB();
+		loading = false;
+	});
+
+	function showToast(data: Toast) {
+		toasts.push(data);
+
+		setTimeout(() => {
+			if (toasts.length) {
+				toasts.pop();
+			}
+		}, 2000);
+	}
+
+	async function deleteTodo(todoId: number) {
+		todos = todos.filter(({ id }) => id !== todoId);
+		await db.todos.delete(todoId);
+
+		showToast({ type: 'success', content: 'You have deleted todo item!' });
+	}
+
+	function addTodo() {
 		if (newTodoName.trim() === '') {
 			return;
 		}
@@ -61,45 +109,101 @@
 			return;
 		}
 
-		document.documentElement.dataset.theme = 'nordemerald';
+		document.documentElement.dataset.theme = 'emerald';
 	});
+
+	$effect(() => {
+		if (todos.length !== savedInDbTodos.length) {
+			if (interval) {
+				clearTimeout(interval);
+			}
+
+			interval = setTimeout(() => {
+				saveToDB();
+			}, 2000);
+		}
+	});
+
+	function handleInputKeydown(e: KeyboardEvent) {
+		if (e.code === 'Enter') {
+			addTodo();
+		}
+	}
 </script>
 
 {#snippet listOption(data: TODO)}
-	<li class="form-control">
+	<li class="form-control w-full">
 		<label class="label cursor-pointer">
 			<input type="checkbox" bind:checked={data.done} class="checkbox checkbox-primary" />
-			<span class="label-text">{data.name}</span>
+			<h3>{data.name}</h3>
+
+			<button class="btn btn-square btn-primary" onclick={() => deleteTodo(data.id)}>
+				<svg
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="none"
+					class="fill-red-400"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path d="M5 7.5H19L18 21H6L5 7.5Z" stroke="#000000" stroke-linejoin="round" />
+					<path
+						d="M15.5 9.5L15 19"
+						stroke="#000000"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					/>
+					<path d="M12 9.5V19" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" />
+					<path d="M8.5 9.5L9 19" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" />
+					<path
+						d="M16 5H19C20.1046 5 21 5.89543 21 7V7.5H3V7C3 5.89543 3.89543 5 5 5H8M16 5L15 3H9L8 5M16 5H8"
+						stroke="#000000"
+						stroke-linejoin="round"
+					/>
+				</svg>
+			</button>
 		</label>
 	</li>
 {/snippet}
 
-<input type="checkbox" class="toggle" bind:checked={isDarkMode} />
+<div class="flex justify-end">
+	<input type="checkbox" class="toggle mt-4 mr-4" bind:checked={isDarkMode} />
+</div>
 
 <section class="flex flex-col">
-	<div class="col col-12">
-		<input class="input input-bordered w-full max-w-xs" placeholder="Enter todo item" type="text" bind:value={newTodoName} />
-		<button class="btn btn-secondary" onclick={saveToDB} disabled={!canSave && !saving}
-			>{saving ? 'Saving...' : 'Save to DB'}</button
-		>
+	<div class="flex w-full justify-center items-center mt-10 gap-4">
+		<input
+			class="input input-bordered w-full max-w-xs"
+			placeholder="Enter todo item"
+			type="text"
+			onkeydown={handleInputKeydown}
+			bind:value={newTodoName}
+		/>
 	</div>
-	{#if todos.length}
-		<div class="col col-12 col-md-6">
-			<h1>TODO</h1>
-			<ul class="list">
-				{#each notDone as todo}
-					{@render listOption(todo)}
-				{/each}
-			</ul>
-		</div>
-		<div class="col col-12 col-md-6">
-			<h1>Done</h1>
-			<ul class="list">
-				{#each done as todo}
-					{@render listOption(todo)}
-				{/each}
-			</ul>
-		</div>
-		
-	{/if}
+	<div class="flex justify-center gap-4 p-4">
+		{#if todos.length && !loading}
+			<div class="mt-10 w-full md:w-1/2">
+				<h2 class="text-center">TODO</h2>
+				<ul class="w-full">
+					{#each notDone as todo}
+						{@render listOption(todo)}
+					{/each}
+				</ul>
+			</div>
+			<div class="mt-10 w-full md:w-1/2">
+				<h2 class="text-center">Done</h2>
+				<ul class="list">
+					{#each done as todo}
+						{@render listOption(todo)}
+					{/each}
+				</ul>
+			</div>
+		{:else if loading}
+			<div class="loading loading-dots loading-lg m-auto"></div>
+		{:else}
+			<h2 class="text-center">No Todo Items added!</h2>
+		{/if}
+	</div>
+
+	<Toasts {toasts} />
 </section>
