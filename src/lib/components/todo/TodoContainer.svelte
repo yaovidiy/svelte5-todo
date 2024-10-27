4825414ad7e708470d987001bbde64a2aca0312e
Yaroslav Ovdii <yaovdiy@gmail.com>
2024-10-27 22:46:50 +0200

    added detailed info

---
+++ src/lib/components/todo/TodoContainer.svelte
@@ -0,0 +1,99 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { todo } from '$lib/TodoControler.svelte';
+	import { modal } from '$lib/ModalControler.svelte';
+	import type { TODO } from '$lib/db';
+	import TodoItem from './TodoItem.svelte';
+
+	let newTodoName = $state<string>('');
+	let todoItem: TODO = $state({ name: '', done: false, deadline: 0, labels: [] });
+
+	function handleInputKeydown(event: KeyboardEvent) {
+		if (event.key === 'Enter') {
+			todo.add(newTodoName);
+			newTodoName = '';
+		}
+	}
+
+	function handleAddTodo() {
+		todo.add(todoItem);
+
+		todoItem = { name: '', done: false, deadline: 0, labels: [] };
+		modal.close();
+	}
+
+	onMount(async () => {
+		todo.loadFromDB();
+	});
+</script>
+
+{#snippet createModal()}
+	<h3 class="text-xl text-center pb-4 mb-4 border-b">Add new todo item</h3>
+	<div class="grid gap-5 grid-cols-1 md:grid-cols-2">
+		<label class="form-control w-full max-w-xs">
+			<div class="label">
+				<span class="label-text">Todo Item Description</span>
+			</div>
+			<input
+				class="input input-bordered w-full max-w-xs"
+				placeholder="Enter todo item"
+				type="text"
+				onkeydown={handleInputKeydown}
+				bind:value={todoItem.name}
+			/>
+		</label>
+
+		<label class="form-control w-full max-w-xs">
+			<div class="label">
+				<span class="label-text">Todo Item Deadline</span>
+			</div>
+			<input
+				class="input input-bordered w-full max-w-xs"
+				type="date"
+				placeholder="select deadline date"
+				bind:value={todoItem.deadline}
+			/>
+		</label>
+
+		<button onclick={handleAddTodo} class="btn btn-primary col-span-2 text-base font-semibold"
+			>Add</button
+		>
+	</div>
+{/snippet}
+
+<section class="flex flex-col">
+	<div class="flex w-full justify-center items-center mt-10 gap-4">
+		<input
+			class="input input-bordered w-full max-w-xs"
+			placeholder="Enter todo item"
+			type="text"
+			onkeydown={handleInputKeydown}
+			bind:value={newTodoName}
+		/>
+		<button onclick={() => modal.open({ content: createModal })}>Show extended params</button>
+	</div>
+	<div class="flex justify-center gap-4 p-4">
+		{#if todo.items.length && !todo.isLoading}
+			<div class="mt-10 w-full md:w-1/2">
+				<h2 class="text-center">TODO</h2>
+				<ul class="w-full">
+					{#each todo.unDoneItems as undone}
+						<TodoItem data={undone} />
+					{/each}
+				</ul>
+			</div>
+			<div class="mt-10 w-full md:w-1/2">
+				<h2 class="text-center">Done</h2>
+				<ul class="list">
+					{#each todo.doneItems as done}
+						<TodoItem data={done} />
+					{/each}
+				</ul>
+			</div>
+		{:else if todo.isLoading}
+			<div class="loading loading-dots loading-lg m-auto"></div>
+		{:else}
+			<h2 class="text-center">No Todo Items added!</h2>
+		{/if}
+	</div>
+</section>
---
+++ src/lib/components/todo/TodoItem.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+	import type { TODO } from '$lib/db';
+	import { todo } from '$lib/TodoControler.svelte';
+	import { modal } from '$lib/ModalControler.svelte';
+
+	let { data }: { data: TODO } = $props();
+
+	function transforDate(date: number | string) {
+		return new Intl.DateTimeFormat('en-US', {
+			year: 'numeric',
+			month: 'long',
+			day: 'numeric'
+		}).format(new Date(date));
+	}
+</script>
+
+{#snippet detailsModal()}
+	<h3 class="text-xl text-center pb-4 mb-4 border-b">Todo Item Details</h3>
+	<div class="grid mb-4 pb-10 border-b gap-5 grid-cols-1 md:grid-cols-2">
+		<label class="form-control w-full max-w-xs">
+			<div class="label">
+				<span class="label-text">Todo Item Description</span>
+			</div>
+			<input
+				class="input input-bordered w-full max-w-xs"
+				placeholder="Enter todo item"
+				type="text"
+				bind:value={data.name}
+			/>
+		</label>
+		<label class="form-control w-full max-w-xs">
+			<div class="label">
+				<span class="label-text">Todo Item Deadline</span>
+			</div>
+			<span class="text-base text-gray-500"
+				>{transforDate(data?.deadline ?? new Date().getTime())}</span
+			>
+			<input type="hidden" />
+		</label>
+	</div>
+	<div class="flex gap-5 justify-center items-center">
+		<button
+			onclick={() => {
+				todo.update(data);
+				modal.close();
+			}}
+			class="btn btn-primary">Update</button
+		>
+		<button
+			onclick={() => {
+				todo.remove(data.id!);
+				modal.close();
+			}}
+			class="btn btn-error">Delete</button
+		>
+	</div>
+{/snippet}
+
+<li class="form-control w-full">
+	<label class="label cursor-pointer">
+		<input
+			type="checkbox"
+			bind:checked={data.done}
+			onchange={() => todo.toggleDone()}
+			class="checkbox checkbox-primary"
+		/>
+		<h3 class={data.done ? 'line-through' : ''}>{data.name}</h3>
+
+		<div class="flex justify-center items-center gap-3">
+			<button onclick={() => modal.open({ content: detailsModal })} class="btn btn-info"
+				>Details</button
+			>
+		</div>
+	</label>
+</li>
